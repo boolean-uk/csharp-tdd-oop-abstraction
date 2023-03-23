acf8d28e759dd849168dfcbae136a2e01d18f25a
john <johnny.lioup@gmail.com>
2023-03-23 10:04:27 +0000

    added base exercise

---
+++ tdd-oop-abstraction.CSharp.Main/Core.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_abstraction.CSharp.Main
+{
+    public class TodoItem {
+        public String title;
+        public String detail;
+        public String status;
+
+        public TodoItem(String title, String detail, String status) {
+            this.title = title;
+            this.detail = detail;
+            this.status = status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getStatus() {
+            return this.status;
+        }
+    }
+}
