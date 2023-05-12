727d37c81ad83a749b1de97757078d612042b221
StavrosSpil <spiliopoulos.stavros@hotmail.com>
2023-05-12 13:38:31 +0300

    Completed with abstraction

---
+++ tdd-oop-abstraction.CSharp.Main/Account.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_abstraction.CSharp.Main
+{
+    public class Account
+    {
+        private bool isActivated;
+        private string email;
+        private string password;
+
+        public Account()
+        {
+            this.isActivated = false;
+        }
+
+        public string CreateAccount(User user,string email, string password)
+        {
+            if (user.Role.Equals("User"))
+            {
+                bool emailResult = CreateEmail(email);
+                bool passwordResult = CreatePassword(password);
+                if (emailResult == true && passwordResult == true)
+                {
+                    return "Account has been created successfully";
+                }
+            }
+            return "Account has not been created";
+        }
+
+        private bool CreateEmail(string email)
+        {
+            if (email.Contains("@"))
+            {
+                this.email = email;
+                return true;
+            }
+            return false;
+        }
+
+        private bool CreatePassword(string password)
+        {
+            if (password.Length > 8)
+            {
+                this.password = password;
+                return true;
+            }
+            return false;
+        }
+
+        private bool ableToLogIn()
+        {
+            if (this.isActivated)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AbleToLogIn(User user)
+        {
+            if (user.Role.Contains("User"))
+            {
+                return ableToLogIn();
+            }
+            return false;
+        }
+
+        private void enableAccount ()
+        {
+            this.IsActivated = true;
+        }
+
+        public bool EnableAccount(User user)
+        {
+            if (user.Role.Contains("Admin"))
+            {
+                enableAccount();
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsActivated { get => this.isActivated; set => this.isActivated = value; }
+    }
+}
--- tdd-oop-abstraction.CSharp.Test/CoreTests.cs
+++ tdd-oop-abstraction.CSharp.Test/CoreTests.cs
@@ -1,13 +1,55 @@
-﻿using tdd_oop_abstraction.CSharp.Main;
+﻿using NUnit.Framework;
+using System.Security.Cryptography.X509Certificates;
+using tdd_oop_abstraction.CSharp.Main;
+
 
 namespace Boolean.CSharp.Test
 {
     public class CoreTests
     {
-        private Core _core;
         public CoreTests()
         {
-            _core = new Core();
+
+        }
+
+        [TestCase("Stavros_1996@windowslive.com", "123456789", "Account has been created successfully")]
+        [TestCase("Stavros_1996windowslive.com", "123456789", "Account has not been created")]
+        [TestCase("Stavros_1996@windowslive.com", "123", "Account has not been created")]
+        public void CreateAccountTest(string email, string password, string expected)
+        {
+            Account account = new Account();
+            User user = new User("User");
+            string result = account.CreateAccount(user, email, password);
+
+            Assert.AreEqual(result, expected);   
+        }
+
+        [Test]
+        public void EnableAccountTest()
+        {
+            Account account = new Account();
+            User user = new User("User");
+            User admin = new User("Admin");
+
+            account.CreateAccount(user, "Stavros_1996@windowslive.com", "123456789");
+            bool result = account.EnableAccount(admin);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(account.IsActivated, true);
+        }
+
+        [Test]
+        public void AbleToLogInTest()
+        {
+            Account account = new Account();
+            User user = new User("User");
+            User admin = new User("Admin");
+
+            account.CreateAccount(user, "Stavros_1996@windowslive.com", "123456789");
+            bool result = account.AbleToLogIn(user);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(account.IsActivated, false);
         }
     }
 }
