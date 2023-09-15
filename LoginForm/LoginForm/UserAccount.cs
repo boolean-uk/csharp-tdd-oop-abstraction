47738d6eecd3c182acca9603972eaacadfeaba0c
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-15 13:51:03 +0200

    added simple windows form

---
+++ LoginForm/LoginForm/UserAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginForm
+{
+    public class UserAccount
+    {
+        private string email;
+        private string password;
+        private bool isAccountEnabled;
+        private string validationMessage;
+
+        public UserAccount(string email)
+        {
+            this.email = email;
+            isAccountEnabled = false;
+            validationMessage = string.Empty;
+        }
+
+        public string ValidationMessage
+        {
+            get { return validationMessage; }
+        }
+
+        public void SetPassword(string password)
+        {
+            this.password = password;
+            Validate();
+        }
+
+        public bool IsThePasswordValid()
+        {
+            return !string.IsNullOrEmpty(password) && password.Length >= 8;
+        }
+
+        public void EnableAccount()
+        {
+            isAccountEnabled = true;
+        }
+
+        public void DisableAccount()
+        {
+            isAccountEnabled = false;
+        }
+
+        public bool IsTheAccountEnableOrDisable()
+        {
+            return isAccountEnabled;
+        }
+
+        public bool IsEmailValid()
+        {
+            return email.Contains("@");
+        }
+
+        private void Validate()
+        {
+            if (!IsEmailValid() || !IsThePasswordValid())
+            {
+                validationMessage = "Invalid email or password. Please try again.";
+            }
+            else
+            {
+                validationMessage = "Account created successfully.";
+            }
+        }
+    }
+}
