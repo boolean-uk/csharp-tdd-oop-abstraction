7696014f5d64edf88093141f561a7bae163d3070
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-15 09:33:30 +0200

    refactor core code

--- tdd-oop-abstraction.CSharp.Main/TodoItem.cs
+++ tdd-oop-abstraction.CSharp.Main/TodoItem.cs
@@ -8,22 +8,17 @@ namespace tdd_oop_abstraction.CSharp.Main
 {
     public class TodoItem {
         
-        public string title;
-        public string detail;
-        public string status;
+        private string title;
+        private string detail;
+        private string status;
 
         public TodoItem(String title, String detail, String status) {
             this.title = title;
             this.detail = detail;
             this.status = status;
         }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getStatus() {
-            return this.status;
-        }
+        public String Title { get { return title; } set { title = value; } }
+        public String Detail { get { return detail; } set { detail = value; } }
+        public String Status { get { return status; } set { status = value; } }
     }
 }
