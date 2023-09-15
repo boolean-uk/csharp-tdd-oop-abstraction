0ed8ba242dc62c5ecb731e8813e940452e8e7ea6
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-15 10:54:56 +0200

    domain model with tests and changed core

--- tdd-oop-abstraction.CSharp.Main/TodoItem.cs
+++ tdd-oop-abstraction.CSharp.Main/TodoItem.cs
@@ -11,14 +11,17 @@ namespace tdd_oop_abstraction.CSharp.Main
         private string title;
         private string detail;
         private string status;
+        private readonly DateTime createdDateTime;
 
         public TodoItem(String title, String detail, String status) {
             this.title = title;
             this.detail = detail;
             this.status = status;
+            createdDateTime = DateTime.Now;
         }
         public String Title { get { return title; } set { title = value; } }
         public String Detail { get { return detail; } set { detail = value; } }
         public String Status { get { return status; } set { status = value; } }
+        public DateTime CreatedDateTime { get { return createdDateTime; } }
     }
 }
