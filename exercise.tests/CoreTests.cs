f5bb591db92b849df04b54efd95dca394a988b1f
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-01-17 12:13:46 +0200

    core and extension tests done

--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -16,11 +16,6 @@ public class CoreTests
         _tditem = new TodoItem("A", "B", "D");
     }
 
-    [Test]
-    public void Test()
-    {
-        Assert.Pass();
-    }
 
     [Test]
     public void CanGetDateTime()
@@ -30,23 +25,6 @@ public class CoreTests
         string date = dt.Date.ToString();
 
         Assert.AreEqual(date, "17/01/2024 0.00.00");
-
-    }
-
-    [Test]
-    public void CannotModifyDateTime()
-    {
-        DateTime dt = _tditem.getDateTime();
-        DateTime dtcomp = _tditem.getDateTime();
-
-        string date = dt.ToString();
-
-        dt = DateTime.Now;
-
-        string date2 = dt.ToString();
-
-        Assert.AreEqual(date, dt.ToString());
-        Assert.AreNotEqual(date2, dt.ToString());
 
     }
 
--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using exercise.main;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,73 @@ namespace exercise.tests
     [TestFixture]
     public class ExtensionTests
     {
+        private User _user;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _user = new User();
+        }
+
         [Test]
         public void Tests()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void PassWordValid()
+        {
+            string res1 = _user.passWordCreate("asonroeinoit");
+
+            Assert.AreEqual(res1, "success!");
+
+            string res2 = _user.passWordCreate("as");
+
+            Assert.AreEqual(res2, "invalid!");
+
+        }
+
+        [Test]
+        public void EmailValid()
+        {
+            string res1 = _user.EmailCreate("asonroeinoit@gmail.com");
+
+            Assert.AreEqual(res1, "success!");
+
+            string res2 = _user.EmailCreate("as");
+
+            Assert.AreEqual(res2, "invalid!");
+
+        }
+
+        [Test]
+        public void NewDisabled()
+        {
+            User us1 = new User();
+
+            Assert.IsFalse(us1.getStatus());
+
+            us1.setAccountEnabled();
+
+            Assert.IsTrue(us1.getStatus());
+
+            us1.setAccountInabled();
+
+            Assert.IsFalse(us1.getStatus());
+        }
+
+        [Test]
+        public void CanUserLogin()
+        {
+            User us1 = new User();
+
+            Assert.IsFalse(us1.CanUserLogin());
+
+            us1.setAccountEnabled();
+
+            Assert.IsTrue(us1.CanUserLogin());
+
         }
     }
 }
