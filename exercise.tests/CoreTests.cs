3f7e900a8e6267393282aaddc090e4e34077d5db
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-17 14:09:15 +0100

    Implement test for TodoItem status

--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -1,11 +1,16 @@
+using exercise.main;
+
 namespace exercise.tests;
 
 [TestFixture]
 public class CoreTests
 {
     [Test]
-    public void Test()
+    public void CanChangeStatusOfTaskTest()
     {
-        Assert.Pass();
+        TodoItem item = new("First Task", "Some task detail", Status.Unfinished);
+
+        item.Status = Status.Finished;
+        Assert.That(item.Status, Is.EqualTo(Status.Finished));
     }
 }
