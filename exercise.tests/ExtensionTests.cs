212624f946f83c739f1e8b20b93ad2b875548f3e
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-17 10:11:51 +0100

    Add tests for Extension

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,18 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ExtensionTests.cs
+using exercise.main;
 
 namespace exercise.tests
 {
-    [TestFixture]
     public class ExtensionTests
     {
-        [Test]
-        public void Tests()
+        private UserAccount _userAccount;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.Pass();
+            _userAccount = new UserAccount("test@example.com" , "password123");
+        }
+
+        [Test]
+        public void TestUserAccountCreation()
+        {
+            var email = "test@example.com";
+            var password = "password123";
+
+            var userAccount = new UserAccount(email , password);
+
+            Assert.IsFalse(userAccount.CanLogin());
+        }
+
+        [Test]
+        public void TestUserAccountEnableDisable()
+        {
+            Assert.IsTrue(_userAccount.Enable());
+            Assert.IsTrue(_userAccount.CanLogin());
+
+            Assert.IsFalse(_userAccount.Enable()); // It should return false as the account is already enabled
+
+            Assert.IsTrue(_userAccount.Disable());
+            Assert.IsFalse(_userAccount.CanLogin());
+
+            Assert.IsFalse(_userAccount.Disable()); // It should return false as the account is already disabled
+        }
+
+        [Test]
+        public void TestInvalidEmail()
+        {
+            var email = "invalid_email";
+            var password = "password123";
+
+            Assert.Throws<Exception>(() => new UserAccount(email , password));
+        }
+
+        [Test]
+        public void TestInvalidPassword()
+        {
+            var email = "test@example.com";
+            var password = "short";
+
+            Assert.Throws<Exception>(() => new UserAccount(email , password));
         }
     }
 }
