77f0bb7c8d86b91ac10f9be435f278c2730960ce
skjalg <spectraldesign1@gmail.com>
2024-01-17 14:36:51 +0100

    Test createAccount

---
+++ exercise.main/User.cs
@@ -0,0 +1,11 @@
+﻿
+namespace exercise.main
+{
+    public class User
+    {
+        public bool createAccount(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,4 +1,6 @@
-﻿namespace exercise.tests
+﻿using exercise.main;
+
+namespace exercise.tests
 {
     [TestFixture]
     public class ExtensionTests
@@ -6,7 +8,20 @@
         [Test]
         public void TestCreateUser()
         {
-            Assert.Pass();
+            //setup
+            User user = new();
+            User user2 = new();
+            User user3 = new();
+
+            //execute
+            bool shouldWork = user.createAccount("some@email.com", "somePasswordOfLengthGreaterThan8");
+            bool shouldNotWorkTooShortPassword = user.createAccount("some@email.com", "somePwd");
+            bool shouldNotWorkNoAt = user.createAccount("noemail.com", "somePasswordOfLengthGreaterThan8");
+
+            //verify
+            Assert.That(shouldWork, Is.True);
+            Assert.That(shouldNotWorkTooShortPassword, Is.False);
+            Assert.That(shouldNotWorkNoAt, Is.False);
         }
     }
 }
