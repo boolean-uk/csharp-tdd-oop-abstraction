baad4a09f83b751716e62c1e1286e79ac1bf5b8e
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-01-18 13:31:11 +0100

    tests done

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -10,9 +10,30 @@ namespace exercise.tests
     public class ExtensionTests
     {
         [Test]
-        public void Tests()
+        public void TestsInvalidPassword()
         {
-            Assert.Pass();
+            var ex = Assert.Throws<ArgumentException>(() => new UserAccount("hotmail@hotmail.com", "1234567"));
+            Assert.That(ex.Message, Is.EqualTo("You must provide a password of at least 8 characters"));
         }
+
+        [Test]
+        public void TestsInvalidEmail() 
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new UserAccount("hotmail.hotmail.com", "12345678"));
+            Assert.That(ex.Message, Is.EqualTo("Invalid email"));
+        }
+
+        public void TestValidAccountCreation()
+        {
+            UserAccount user = new UserAccount("hotmail@hotmail.com", "12345678");
+            Assert.IsInstanceOf(UserAccount, user);
+        }
+
+        public void TestUserAccountDisabledOnCreate() 
+        {
+            UserAccount user = new UserAccount("hotmail@hotmail.com", "12345678");
+            Assert.That(user.enabled == false);
+        }
+
     }
 }
