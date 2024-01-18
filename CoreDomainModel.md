184e91f6fda3f287484880f6a7106a010835a7a8
Sebastian <sebastian.hanssen@outlook.com>
2024-01-18 15:00:44 +0100

    add core Domain Model

---
+++ CoreDomainModel.md
@@ -0,0 +1,6 @@
+```
+I want to see a Date and Time the task was created but I do not want it changed.   
+```
+| Classes  | Methods           | Function                  | Scenario | Output   |
+|----------|-------------------|---------------------------|----------|----------|
+| ToDoItem | getCreationDate() | Returns the creation date |          | DateTime |
--- exercise.main/TodoItem.cs
+++ exercise.main/TodoItem.cs
@@ -9,25 +9,33 @@ namespace exercise.main
     public class TodoItem
     {
 
-        public string title;
-        public string detail;
-        public string status;
+        private string title;
+        private string detail;
+        private bool status;
+        private DateTime creationdate;
 
-        public TodoItem(String title, String detail, String status)
+        public TodoItem(string title, string detail, bool status)
         {
             this.title = title;
             this.detail = detail;
             this.status = status;
+            this.creationdate = DateTime.Now;
         }
 
-        public void setStatus(String status)
+        public void toggleStatus()
         {
-            this.status = status;
+            this.status = !status;
         }
 
-        public String getStatus()
+        public string getStatus()
         {
-            return this.status;
+            if (this.status == false)
+            {
+                return "Uncompleted";
+            } else
+            {
+                return "Completed";
+            }
         }
     }
 }
