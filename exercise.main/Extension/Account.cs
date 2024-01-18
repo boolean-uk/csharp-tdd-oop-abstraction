b6f33514108145a0b010e7ee89975c2bdd752988
Kanthee Kosolyuthasarn <Kanthee>
2024-01-18 14:01:33 +0100

    Kanthee Kosolyuthasarn - C#3 - 18.01.2024: commit Extension 4.0

--- exercise.main/Extension/Account.cs
+++ exercise.main/Extension/Account.cs
@@ -1,6 +1,48 @@
-﻿namespace exercise.tests
+﻿
+namespace exercise.main.Extension
 {
-    internal class Account
-    {
+    public class Account
+    {   // List of users
+        protected Dictionary<string, bool> _Users = new Dictionary<string, bool>();
+
+
+
+        public bool makeAccount(string userName, string passWord)
+        {
+            //Perform the check for valid password and userName:
+            if (checkPassword(passWord) && checkUser(userName))
+            {
+                _Users.Add(userName, false);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool checkPassword(string passWord)
+        {
+            if (passWord.Length > 8)
+            {  //If the condition is not met -> false
+                return true;
+            }
+            return false;
+        }
+
+        public bool checkUser(string userName)
+        {
+            if (userName.Contains('@'))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool getAccountStatus(string userName)
+        {
+            return _Users[userName];
+        }
     }
 }
--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,4 +1,5 @@
 ﻿using exercise.main;
+using exercise.main.Extension;
 using System;
 using System.Collections.Generic;
 using System.Linq;
