4581bc085c8840cee41a8936f31f4ac9dad22b33
Kanthee Kosolyuthasarn <Kanthee>
2024-01-18 11:58:58 +0100

    Kanthee Kosolyuthasarn - C#3 - 18.01.2024: commit Extension 2.0

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -26,5 +26,25 @@ namespace exercise.tests
             // Assert
             Assert.IsTrue(status == true);  //True if the account was created.
         }
+
+        [Test]
+        public void testCheckPassword()
+        {
+
+            // Arrange
+            string userName = "1234@hotmail.com";
+            string passWord1 = "12345";
+            string passWord2 = "123456789";
+
+            Account accounts = new Account();
+
+            // Act
+            bool status1 = accounts.checkPassword(passWord1);
+            bool status2 = accounts.checkPassword(passWord2);
+
+            // Assert
+            Assert.IsTrue(status1 == true);  //Should returns true 
+            Assert.IsTrue(status2 == false);  //Should returns false
+        }
     }
 }
