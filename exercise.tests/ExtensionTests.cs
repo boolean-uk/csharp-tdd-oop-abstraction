c9c5841df6ee2eb4c6c9b8529d15c61f7c98b82b
Elsa Lossius <elsa.lossius@no.experis.com>
2024-01-18 13:47:59 +0100

    Green extension tests

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using exercise.main;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,60 @@ namespace exercise.tests
     public class ExtensionTests
     {
         [Test]
-        public void Tests()
+        public void SholdNotBePossibleToCreateUserWithInvalidPassword()
         {
-            Assert.Pass();
+            string email = "elsalos@icloud.com";
+            string password = "1234567";
+            string response = "";
+            try
+            {
+                User user = new User(email, password);
+            } catch (Exception ex)
+            {
+                response = ex.Message;
+            }
+            
+            Assert.That(response, Is.EqualTo("invalid password"));
+        }
+        [Test]
+        public void SholdNotBePossibleToCreateUserWithInvalidEmail()
+        {
+            string email = "elsalosicloud.com";
+            string password = "12345678";
+            string response = "";
+            try
+            {
+                User user = new User(email, password);
+            }
+            catch (Exception ex)
+            {
+                response = ex.Message;
+            }
+
+            Assert.That(response, Is.EqualTo("invalid email"));
+        }
+        [Test]
+        public void SholdBePossibleToCreateUserWithValidEmailAndPassword()
+        {
+            User user = new User("elsalos@icloud.com", "12345678");
+            Assert.That(user.Email, Is.EqualTo("elsalos@icloud.com"));
+        }
+        [Test]
+        public void SholdNotLoginIfDisabled()
+        {
+            User user = new User("elsalos@icloud.com", "12345678");
+            bool loggedIn = user.Login();
+
+            Assert.IsFalse(loggedIn);
+        }
+        [Test]
+        public void SholdLoginIfEnabled()
+        {
+            User user = new User("elsalos@icloud.com", "12345678");
+            user.Enable();
+            bool loggedIn = user.Login();
+
+            Assert.IsTrue(loggedIn);
         }
     }
 }
