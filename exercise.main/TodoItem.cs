7d2924de993959dc92c5c4392b0031516b064d1d
Najem Hamo <najem.hamo@gmail.com>
2024-01-19 13:51:52 +0100

    core

--- exercise.main/TodoItem.cs
+++ exercise.main/TodoItem.cs
@@ -1,33 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace exercise.main
 {
+    public enum TodoItemStatus
+    {
+        DONE, 
+        NOT_DONE
+    }
+
     public class TodoItem
     {
+        /*
+            User story: I want to see a Date and Time the task was created but I do not want it changed.
+        */
 
-        public string title;
-        public string detail;
-        public string status;
+        private string title, description;
+        private TodoItemStatus status;
+        public string Title {get { return title; }}
+        public string Description {get { return description; }}
+        public TodoItemStatus Status {get { return status; }}
 
-        public TodoItem(String title, String detail, String status)
+        public TodoItem(String title, String description, TodoItemStatus status)
         {
             this.title = title;
-            this.detail = detail;
-            this.status = status;
+            this.description = description;
+            this.status = status;                                           
         }
 
-        public void setStatus(String status)
+        public void MarkAsDone()
         {
-            this.status = status;
+            this.status = TodoItemStatus.DONE;
         }
 
-        public String getStatus()
-        {
-            return this.status;
-        }
     }
 }
