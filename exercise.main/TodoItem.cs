aeaec07fc57015034b1f266e22a8035ec170b68d
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-19 09:46:15 +0100

    Core done

--- exercise.main/TodoItem.cs
+++ exercise.main/TodoItem.cs
@@ -9,15 +9,19 @@ namespace exercise.main
     public class TodoItem
     {
 
-        public string title;
-        public string detail;
-        public string status;
+        private string title;
+        private string detail;
+        private string status;
+        private string date;
+        private string time;
 
-        public TodoItem(String title, String detail, String status)
+        public TodoItem(String title, String detail, String status, string date, string time)
         {
             this.title = title;
             this.detail = detail;
             this.status = status;
+            this.date = date;
+            this.time = time;
         }
 
         public void setStatus(String status)
@@ -28,6 +32,32 @@ namespace exercise.main
         public String getStatus()
         {
             return this.status;
+        }
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+
+        public String getTitle()
+        {
+            return this.title;
+        }
+        public void setDetail(String detail)
+        {
+            this.detail = detail;
+        }
+
+        public String getDetail()
+        {
+            return this.detail;
+        }
+        public String getDate()
+        {
+            return this.date;
+        }
+        public String getTime()
+        {
+            return this.time;
         }
     }
 }
