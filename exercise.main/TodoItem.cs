352c047dacdf3eddd8822f6ede6d6d39c0945b9a
Mathias Fredriksen <mathias.fredriksen@no.experis.com>
2024-01-19 09:56:41 +0100

    TodoItem complete

--- exercise.main/TodoItem.cs
+++ exercise.main/TodoItem.cs
@@ -6,28 +6,44 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
+    public enum StatusState
+    {
+        Complete,
+        Incomplete
+    }
     public class TodoItem
     {
+        
+        protected string title { get; set; }
+        protected string detail { get; set; }
+        protected string status { get; set; }
+        // NOTE: Alternatively "public string status { get; protected set; }" could be used instead, in order to remove the need for methods
 
-        public string title { get; protected set; }
-        public string detail { get; protected set; }
-        public string status { get; protected set; }
-
-        public TodoItem(String title, String detail, String status)
+        public TodoItem(String title, String detail, StatusState state = StatusState.Incomplete)
         {
             this.title = title;
             this.detail = detail;
             this.status = status;
         }
 
-        public void setStatus(String status)
+        public void SetStatus(StatusState newStatus)
         {
-            this.status = status;
+            this.status = newStatus.ToString();
         }
 
-        public String getStatus()
+        public String GetStatus()
         {
             return this.status;
+        }
+
+        public String GetDetail()
+        {
+            return this.detail;
+        }
+
+        public String GetTitle()
+        {
+            return this.title;
         }
     }
 }
--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -1,3 +1,4 @@
+using exercise.main;
 using Microsoft.VisualBasic;
 
 namespace exercise.tests;
@@ -13,4 +14,21 @@ public class CoreTests
         Assert.That(dateAndTime.GetDate() == DateTime.Now.ToString("dd.MM.yyyy"));
         Assert.That(dateAndTime.GetTime() == DateTime.Now.ToString("t"));
     }
+
+    [Test]
+    public void StatusTest()
+    {
+        String detail = "Lorem ipsum is a placeholder text commonly used in publishing and graphic design to demonstrate the visual for of a document or a typeface without relying on meaningful content";
+        
+        TodoItem todoItem = new TodoItem("Lorem Ipsum", detail );
+
+        Assert.That(todoItem.GetDetail() == detail);
+        Assert.That(todoItem.GetTitle() == "Lorem Ipsum");
+
+        Assert.That(todoItem.GetStatus() == "Incomplete");
+        todoItem.SetStatus(StatusState.Complete);
+        Assert.That(todoItem.GetStatus() == "Complete");
+
+    }
+
 }
