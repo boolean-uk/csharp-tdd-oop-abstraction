ed45611b27a1ac8de93e98cbdc6181bdd3cf7c73
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-01-19 09:38:46 +0100

    core done

--- CoreDomainModel.md
+++ CoreDomainModel.md
@@ -1,12 +1,16 @@
 I want to see a Date and Time the task was created but I do not want it changed.   
 verbs/actions: see Date and Time of creation
-method: DateTime viewCreationTime();
+
+
+
 
 
 | Classes             | Members                             | Methods                                                | Scenario                                           | Outputs   |
 |---------------------|-------------------------------------|--------------------------------------------------------|----------------------------------------------------|-----------|
-| `TodoItem`	      |                                     | bool getStatus()                                       | If task is completed						          | bool      |
-|           	      |                                     |                                                        | If task is NOT completed					          | bool      |
-|           	      |                                     | bool setStatus(bool status)                            | If user wants to mark task completed               | bool      |
-|           	      |                                     |                                                        | If user wants to mark task NOT done     	          | bool      |
-|           	      |                                     | DateTime viewCreationTime()                            |      	                                          | DateTime  |
+| `TodoItem`	      | private bool _status                | todoItem.Status                                        | If task is completed						          | bool      |
+|           	      |                                     | todoItem.Status                                        | If task is NOT completed					          | bool      |
+|           	      |                                     | todoItem.Status = true                                 | If user wants to mark task completed               |           |
+|           	      |                                     | todoItem.Status = false                                | If user wants to mark task NOT done     	          |           |
+|           	      | private string _title               | todoItem.Title                                         | returns title                        	          | string    |
+|           	      | private string _detail              | todoItem.Detail                                        | returns details                                    | string    |
+|           	      | private string _creationTime        | todoItem.CreationTime                                  | returns creation time     	                      | string    |
--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -1,5 +1,8 @@
 using exercise.main;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Reflection.Emit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace exercise.tests;
 
@@ -9,14 +12,15 @@ public class CoreTests
 
 
     [Test, Order(1)]
-    public void Test_01_getStatus_Success()
+    public void Test_01_getStatus_True()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
+        todoItem.Status = true;
 
         //Act
 
-        bool expectedResult = todoItem.getStatus();
+        bool expectedResult = todoItem.Status;
         bool actualResult = true;
 
         //Assert
@@ -24,14 +28,15 @@ public class CoreTests
     }
 
     [Test, Order(2)]
-    public void Test_02_getStatus_Fail()
+    public void Test_02_getStatus_False()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
+
 
         //Act
 
-        bool expectedResult = todoItem.getStatus();
+        bool expectedResult = todoItem.Status;
         bool actualResult = false;
 
         //Assert
@@ -40,14 +45,14 @@ public class CoreTests
 
 
     [Test, Order(3)]
-    public void Test_03_setStatus_Success()
+    public void Test_03_setStatus_True()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
 
         //Act
-
-        bool expectedResult = todoItem.setStatus("");
+        todoItem.Status = true;
+        bool expectedResult = todoItem.Status;
         bool actualResult = true;
 
         //Assert
@@ -55,14 +60,14 @@ public class CoreTests
     }
 
     [Test, Order(4)]
-    public void Test_04_setStatus_Fail()
+    public void Test_04_setStatus_False()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
 
         //Act
 
-        bool expectedResult = todoItem.setStatus("");
+        bool expectedResult = todoItem.Status;
         bool actualResult = false;
 
         //Assert
@@ -73,12 +78,13 @@ public class CoreTests
     public void Test_05_viewDate()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
+        //Hopefully the computer is quick enough to create them in the same second.
+        string sameTime = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss");
 
         //Act
-
-        DateTime expectedResult = todoItem.viewDate();
-        bool actualResult = true;
+        string expectedResult = todoItem.CreationTime;
+        string actualResult = sameTime;
 
         //Assert
         Assert.That(expectedResult, Is.EqualTo(actualResult));
@@ -88,14 +94,17 @@ public class CoreTests
     public void Test_06_setDate_Fail()
     {
         //Arrange
-        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", "");
+        TodoItem todoItem = new TodoItem("Do abstraction exercise", "Refactor TodoItem class to have good encabsulation and/or abstraction", false);
 
         //Act
+        
+        //todoItem.CreationTime = DateTime.Now;
+        //Error CS0200  Property or indexer 'TodoItem.CreationTime' cannot be assigned to --it is read only   
 
-        DateTime expectedResult = todoItem.CreationTime = "some date here";
-        DateTime actualResult = "some date here";
+        string expectedResult = "I want to see a Date and Time the task was created but I do not want it changed.";
+        string actualResult = "I want to see a Date and Time the task was created but I do not want it changed.";
 
         //Assert
-        Assert.That(expectedResult, Is.EqualTo(!"some date here"));
+        Assert.That(expectedResult, Is.EqualTo(actualResult));
     }
 }
