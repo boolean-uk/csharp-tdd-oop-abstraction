c6098d6587cfe4d51d41898315eda030575d6ef3
Sebastian <sebastian.hanssen@outlook.com>
2024-01-19 10:29:57 +0100

    add extension tests

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using exercise.main;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,79 @@ namespace exercise.tests
     public class ExtensionTests
     {
         [Test]
-        public void Tests()
+        public void CreateUser()
         {
-            Assert.Pass();
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssen@outlook.com", "password123");
+            //Verify
+            Assert.IsNotNull(system.userList);
+            Assert.That(system.userList[0].Username, Is.EqualTo("sebastian.hanssen@outlook.com"));
+        }
+
+        [Test]
+        public void CreateUserEmailError()
+        {
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssenoutlook.com", "password123");
+            //Verify
+            Assert.That(system.errorMessage, Is.EqualTo("Invalid E-mail."));
+        }
+
+        [Test]
+        public void CreateUserPasswordError()
+        {
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssen@outlook.com", "pass");
+            //Verify
+            Assert.That(system.errorMessage, Is.EqualTo("Invalid Password."));
+        }
+
+        [Test]
+        public void SignInFail()
+        {
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssen@outlook.com", "password123");
+            system.userList[0].signIn();
+            //Verify
+            Assert.IsNotNull(system.userList);
+            Assert.That(system.userList[0].Username, Is.EqualTo("sebastian.hanssen@outlook.com"));
+            Assert.That(system.errorMessage, Is.EqualTo("Account not enabled."));
+            Assert.That(system.userList[0].Status, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void SignIn()
+        {
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssen@outlook.com", "password123");
+            system.admin.toggleUser(userList[0]);
+            system.userList[0].signIn();
+            //Verify
+            Assert.IsNotNull(system.userList);
+            Assert.That(system.userList[0].Username, Is.EqualTo("sebastian.hanssen@outlook.com"));
+            Assert.That(system.userList[0].Status, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void SignInNoUserError()
+        {
+            //Set Up
+            System system = new System();
+            //Execute
+            system.createUser("sebastian.hanssen@outlook.com", "password123");
+            system.admin.toggleUser("s.h@outlook.com");
+            //Verify
+            Assert.That(system.errorMessage, Is.EqualTo("There is no user with the e-mail s.h@outlook.com"));
         }
     }
 }
