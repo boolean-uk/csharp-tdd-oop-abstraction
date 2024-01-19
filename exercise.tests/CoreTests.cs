d8d3dac0a42b4398a1158cb78c35148bee2acd4d
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-01-19 12:43:08 +0200

    coretest for userstory 1 and 2 done

--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -1,6 +1,7 @@
 using static System.Formats.Asn1.AsnWriter;
 using NUnit.Framework;
 using exercise.main;
+using System.Net.NetworkInformation;
 
 namespace exercise.tests;
 
@@ -13,9 +14,31 @@ public class CoreTests
     [SetUp]
     public void SetUp()
     {
-        _tditem = new TodoItem("A", "B", "incomplete");
+        _tditem = new TodoItem("A", "B", TodoItemStatus.NOT_DONE);
     }
 
+    [TestCase("title", "descr", TodoItemStatus.NOT_DONE)]
+    [TestCase("title", "descr", TodoItemStatus.DONE)]
+    [TestCase("", "", TodoItemStatus.NOT_DONE)]
+    public void CanCreateTodoItems(string title, string descr, TodoItemStatus status)
+    {
+        TodoItem item = new TodoItem(title, descr, status);
+
+        Assert.That(item.Title, Is.EqualTo(title));
+        Assert.That(item.Description, Is.EqualTo(descr));
+        Assert.That(item.Status, Is.EqualTo(status));
+
+    }
+
+    [TestCase(false, true)]
+    [TestCase(true, true)]
+    public void CanMarkItemAsDone(TodoItemStatus initalstatus, TodoItemStatus finalState)
+    {
+        TodoItem item = new TodoItem("", "", initalstatus);
+        item.setDone();
+
+        Assert.That(item.Status, Is.EqualTo(finalStatus));
+    }
 
     [Test]
     public void CanGetDateTime()
