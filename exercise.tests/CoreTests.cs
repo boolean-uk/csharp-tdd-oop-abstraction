025106522fee2217db10ddd778719bd810a70234
Santhia <santhia.shanmukarasan@gmail.com>
2024-01-19 09:53:39 +0100

    Abstraction core exercise, done

--- exercise.tests/CoreTests.cs
+++ exercise.tests/CoreTests.cs
@@ -1,11 +1,37 @@
+using exercise.main;
+
 namespace exercise.tests;
 
 [TestFixture]
 public class CoreTests
 {
     [Test]
-    public void Test()
+    public void TodoItem_StatusCanBeChanged()
     {
-        Assert.Pass();
+        // Arrange
+        TodoItem todoItem = new TodoItem("Complete Coding Assignment", "Finish the programming tasks", "InProgress");
+
+        //Act
+        todoItem.setStatus("Completed");
+
+        //Assert
+        Assert.AreEqual("Completed", todoItem.getStatus(), "Status should be changed to 'Completed'.");
+
+
     }
+
+    [Test]
+    public void CreationDateTimeShouldBeSetRight()
+    {
+        // Arrange
+        TodoItem todoItem = new TodoItem("Complete Coding Assignment", "Finish the programming tasks", "InProgress");
+
+        //Act
+        DateTime creationDateTime = todoItem.getCreationDateTime();
+
+        //Assert
+        Assert.AreEqual(DateTime.Now.Date, creationDateTime.Date, "Creation date should match the current date.");
+        Assert.AreEqual(DateTime.Now.Hour, creationDateTime.Hour, "Creation hour should match the current hour.");
+        Assert.AreEqual(DateTime.Now.Minute, creationDateTime.Minute, "Creation minute should match the current minute.");    }
+
 }
