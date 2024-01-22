214445b7b6d5641148e9b9c687b87401b2e30f05
Aleksander Kolsrud <kols203@gmail.com>
2024-01-22 15:55:40 +0100

    Now finished, removed TodoItem by accident

--- exercise.main/TodoItem.cs
+++ exercise.main/TodoItem.cs
@@ -6,4 +6,37 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
+    public class TodoItem
+    {
+        private string _title;
+        private string _detail;
+        private bool _status;
+        private DateTime _dateTime;
+
+        public TodoItem(string title, string detail, bool status)
+        {
+            this._title = title;
+            this._detail = detail;
+            this._status = status;
+            this._dateTime = DateTime.Now;
+        }
+        public void changeStatus() 
+        {
+            if(_status)
+            {
+                this._status = false;
+            }
+            this._status = true;
+        }
+
+        public DateTime getDateTime()
+        {
+            return _dateTime;
+        }
+
+        public bool getStatus()
+        {
+            return this._status;
+        }
+    }
 }
